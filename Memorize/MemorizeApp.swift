ee0eedcd5e6ace6b30b3dc8a14d57be671e2af9e
dkhangsar <khangsardylan@gmail.com>
2024-12-29 22:21:02 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dylan Khangsar on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
