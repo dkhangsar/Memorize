37fbd4eb6ff67d27282f64528cf126327579d58d
dkhangsar <khangsardylan@gmail.com>
2024-12-30 20:42:48 -0600

    Created ScrollView and button sizing

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["😈", "🕷️", "👻", "🎃"]
+    let emojis: [String] = ["😈", "🕷️", "👻", "🎃", "😱", "👹", "🧙", "☠️", "🍭", "🍬", "🐈‍⬛"]
     // array<String> == [String] SAME!
+    
+    @State var cardCount: Int = 4
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()  // pads only the entire VStack, not everything inside
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack{  // considering that this is technically one line of code, there is an implicit return here
+            cardRemover
+            Spacer() // uses up all the space in between these two
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum:120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content:emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        // syntax of by offset -- by is used external user, offset used by internal
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
     }
 }
 
@@ -30,13 +71,13 @@ struct CardView: View {
         ZStack {  // allowed to not have () because trailing closure syntaax
             let base = RoundedRectangle(cornerRadius: 12) // let means that it's a var that is CONSTANT
             // type inference -- don't need to write "RoundedRectangle = RoundedRectangle(corner..."
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content)
-            } else {
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {  // if i wanted a doubel tap, (count: 2)
             isFaceUp.toggle()  // views are immuttable -- so @State = temporary state
