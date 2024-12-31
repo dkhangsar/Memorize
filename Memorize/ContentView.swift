fae5409a4de6d1f215e6934c94b9e4784e496138
dkhangsar <khangsardylan@gmail.com>
2024-12-30 19:53:50 -0600

    Togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,29 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
+            CardView(isFaceUp: false)
+            CardView()
             CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
         }
-        .foregroundColor(.blue)
+        .foregroundColor(.orange)
         .padding()
     }
 }
 
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false  // this needs to be a var because it could change if it passes
+    // we should always want to start with let
     
     var body: some View {
-        ZStack {
+        ZStack {  // allowed to not have () because trailing closure syntaax
+            let base = RoundedRectangle(cornerRadius: 12) // let means that it's a var that is CONSTANT
+            // type inference -- don't need to write "RoundedRectangle = RoundedRectangle(corner..."
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.orange)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("😄")
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {  // if i wanted a doubel tap, (count: 2)
+            print("tapped")
+            isFaceUp.toggle()  // views are immuttable -- so @State = temporary state
         }
     }
 }
